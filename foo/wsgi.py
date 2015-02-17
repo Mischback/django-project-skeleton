e7bf3399338c174a15e00d3833b4f755ebf7afc6
Mischback <dev@mischback.de>
2015-02-17 21:04:15 +0100

    Completed production settings

--- foo/wsgi.py
+++ foo/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foo.settings.production")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
