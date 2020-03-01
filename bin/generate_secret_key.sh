fb7e72ed4393c2e66f1256a606c47db62746c58d
Mischback <dev@mischback.de>
2020-03-01 22:27:47 +0100

    Some more comments on generating SECRET_KEY

--- bin/generate_secret_key.sh
+++ bin/generate_secret_key.sh
@@ -6,6 +6,11 @@
 # Ths script is - obviously - no reimplementation of the corresponding
 # function provided by Django, but should provide a value, that may be
 # considered "random enough".
+# The code is taken from https://gist.github.com/earthgecko/3089509 , see
+# https://github.com/django/django/blob/9386586f31b8a0bccf59a1bff647cd829d4e79aa/django/core/management/utils.py#L77
+# for Django's original implementation.
+#
+# '&' had to be removed from the available characters, because it breaks 'sed'.
 #
 # This script is used to generate a SECRET_KEY in the env.production file.
 # That file is **NOT** under version control, for obvious reasons.
