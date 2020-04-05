dd30df678e4386b09b6295d12208aaa119875270
Mischback <dev@mischback.de>
2020-04-05 21:03:43 +0200

    Get rid of docker-specific settings
    The docker-specific settings were included into the production settings, with
    some documentation, why and how Django's logging is deactivated.
    
    It also includes a fix for ALLOWED_HOSTS, to work nicely in our Docker-based
    deployment, but should be working fine in other setups aswell.

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,11 +8,33 @@ from .common import *
 # ##### APPLICATION CONFIGURATION #########################
 
 # You will have to determine, which hostnames should be served by Django
-ALLOWED_HOSTS = os.environ.get('DPS_DJANGO_ALLOWED_HOSTS', '').split()
+# Determines, which hostnames should be served by Django.
+# DPS_DJANGO_ALLOWED_HOSTS may contain a list of (acceptable) hostnames, while
+# DPS_SERVER_NAME may only contain one hostname, as this value also determines
+# the actual hostname, that is used by Nginx.
+#
+# If neither DPS_DJANGO_ALLOWED_HOSTS nor DPS_SERVER_NAME are provided, this
+# will be an empty list and thus, no host will be served, rendering Django
+# effectively useless.
+ALLOWED_HOSTS = list(
+    set(
+        os.environ.get('DPS_DJANGO_ALLOWED_HOSTS', '').split() +
+        os.environ.get('DPS_SERVER_NAME', '').split()
+    )
+)
 
 INSTALLED_APPS = DEFAULT_APPS
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+# Don't let Django configure logging in a production environment.
+# Most probably, configuring logging should be done by WSGI Server, i.e.
+# Gunicorn or uWSGI.
+# For Docker-based deployments, logging is configured in 'gunicorn_conf.py'.
+#
+# Without setting LOGGING_CONFIG to 'None', Django will setup Python's logging
+# module, as described https://docs.djangoproject.com/en/3.0/topics/logging/#default-logging-configuration
+LOGGING_CONFIG=None
 
 # ##### SECURITY CONFIGURATION ############################
 
--- deploy/Docker/django/run_gunicorn.sh
+++ deploy/Docker/django/run_gunicorn.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-export DJANGO_SETTINGS_MODULE="config.settings.docker"
+export DJANGO_SETTINGS_MODULE="config.settings.production"
 
 exec gunicorn sgi.wsgi:application --config='file:/docker-bin/gunicorn_conf.py'
